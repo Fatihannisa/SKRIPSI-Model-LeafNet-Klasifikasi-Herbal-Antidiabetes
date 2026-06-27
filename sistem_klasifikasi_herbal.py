c993f971c23fc21b7a8c5cce688a990256b3d35e
Fatihannisa Listy Zulmi_034 <114486927+Fatihannisa@users.noreply.github.com>
2026-06-27 18:49:24 +0700

    Update sistem_klasifikasi_herbal.py

--- sistem_klasifikasi_herbal.py
+++ sistem_klasifikasi_herbal.py
@@ -674,15 +674,27 @@ elif st.session_state.page == "result":
         st.markdown(f"""
             <div class="adaptive-box">
                 <b class='section-title'>Status</b><br>
-                <b style="color:#018790; font-weight:400;">{data['status'] if data else "Bukan herbal antidiabetes"}</b><br><br>
-                <b>Tingkat kepercayaan sistem: </b> 
-                <b style="color:#018790;">{conf * 100:.2f}% </b>
+                <b style="color:#018790; font-weight:400;">
+                    {data['status'] if data else "Bukan herbal antidiabetes"}
+                </b><br><br>
+                <b>Tingkat kepercayaan sistem:</b>
+                <b style="color:#018790;">
+                    {conf * 100:.2f}%
+                </b>
             </div>
         """, unsafe_allow_html=True)
+        st.markdown("<div style='height:15px'></div>",
+                    unsafe_allow_html=True)
+        with st.container(border=True):
+        st.markdown(
+            "<b class='section-title'>Top-5 Prediksi</b>",
+            unsafe_allow_html=True
+        )
         st.markdown("<br>", unsafe_allow_html=True)
-        st.markdown("### Top-5 Prediksi")
         for i, (label, score) in enumerate(top5, 1):
-            st.write(f"{i}. {label} — {score*100:.2f}%")
+            st.write(
+                f"{i}. {label} — {score*100:.2f}%"
+            )
             st.progress(float(score))
 
     # === Informasi ===
